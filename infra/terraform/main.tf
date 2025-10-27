8ffc040a3c92651849e1c2f28c63f37ad0307c47
Jonnathan Calderon <jecs-8@hotmail.com>
2025-10-27 12:47:10 -0500

    Cambios Terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,18 +2,8 @@ locals {
   provider = "azure"
 }
 
-terraform {
-  required_providers {
-    azurerm = { source = "hashicorp/azurerm" }
-  }
-
-  # Backend configuration
-  # Configure remote state in CI or by creating a local `backend.tf` (do NOT commit credentials).
-  # Example: terraform init -backend-config="storage_account_name=<sa>" -backend-config="container_name=tfstate" -backend-config="key=envs/azure/${var.env}/terraform.tfstate"
-}
-
 provider "azurerm" {
-  features = {}
+  features {}
 }
 
 module "azure" {
@@ -23,11 +13,11 @@ module "azure" {
   repo_name           = var.repo_name
   resource_group_name = var.resource_group_name
   location            = var.location
+  region              = var.region
   node_count          = var.node_count
   node_size           = var.node_size
   cluster_name        = var.cluster_name
   k8s_version         = var.k8s_version
-  ssh_public_key      = var.ssh_public_key
 }
 
 output "registry" {
--- infra/terraform/modules/azure/variables.tf
+++ infra/terraform/modules/azure/variables.tf
@@ -7,3 +7,7 @@ variable "k8s_version" { type = string }
 variable "repo_name" { type = string }
 variable "resource_group_name" { type = string }
 variable "location" { type = string }
+variable "ssh_public_key" { 
+  type = string
+  default = ""
+}
